c0f23aaacae1a6a61f346bc785c87a650115d3b9
gurpreetkaur230914 <gurpreet.khanuja@gammaedge.io>
2025-03-25 17:37:47 +0530

    added data types

---
+++ data_types.py
@@ -0,0 +1,15 @@
+x= "gurpreet"# string 
+num=1
+complexs=1j
+lists=[2,2,2]
+tuples=(2,3,4)
+ranges=range(6)
+diction={2:"nonu"}
+sets={2,3,4}
+bytes=b"hello"
+byte_array=bytearray(5)
+# memory_view=memoryview(bytes(5))
+print("string is ",x,"num is ",num,"list is ",lists,"tuples is ",tuples,"ranges is ",ranges,"diction is ",diction,"sets is ",sets,"bytes is ",bytes,"byte_array",byte_array,"memory view is ")
+
+
+
